6c75c7a0c1d03e5ac37d622c4b32270e31a99f72
TomasHutnan <tomas.hutnan147@gmail.com>
2024-04-06 20:35:48 +0200

    Update api_nasdaq.ts

--- lib/api_nasdaq.ts
+++ lib/api_nasdaq.ts
@@ -1,5 +1,12 @@
 import { getPortfolio, getTransactions } from "./db_mongo";
 
+export async function getCurrentStockPrice(symbol: string): Promise<number> {
+    const url = `https://api.nasdaq.com/api/quote/${symbol}/info?assetclass=stocks`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.data.primaryData.lastSalePrice;
+}
+
 async function getDailyStockData(symbol: string, fromdate: string, todate: string): Promise<any> {
     // date format: yyyy-mm-dd
     const url = `https://api.nasdaq.com/api/quote/${symbol}/chart?assetclass=stocks&fromdate=${fromdate}&todate=${todate}&api_key=${process.env.NASDAQ_API_KEY}`;
@@ -18,12 +25,16 @@ async function getDailyStockData(symbol: string, fromdate: string, todate: strin
 
 async function getDailyStockValue(_id: string, symbol: string, days: number = 30, ): Promise<any> {}
 
-export async function getDailyPortfolioValue(_id: string, fromdate: string, todate: string): Promise<any> {
+export async function getDailyPortfolioValue(_id: string, days: number = 30): Promise<any> {
     const portfolio = await getPortfolio(_id);
     const transactions = await getTransactions(_id);
-    const from = new Date(fromdate);
+    const todate = new Date();
 
     const portfolioBacktrack: { [key: string]: {[key: string]: number} } = {};
+    for (let i = 0; i < portfolio.length; i++) {
+        portfolioBacktrack[portfolio.symbol] = {[convertDateToString(todate)] : portfolio[i].price, amount: portfolio[i].amount};
+    }
+
     for (let i = 0; i < portfolio.length; i++) {
         const symbol = portfolio[i].symbol;
         const price = portfolio[i].price;
@@ -35,8 +46,8 @@ export async function getDailyPortfolioValue(_id: string, fromdate: string, toda
 
     let portfolioValue = 0;
     for (const [symbol, quantity] of portfolio) {
-        const stockData = await getDailyStockValue(_id, symbol, date, );
-        portfolioValue += stockData[symbol] * quantity;
+        //const stockData = await getDailyStockValue(_id, symbol, date, );
+        //portfolioValue += stockData[symbol] * quantity;
     }
     return portfolioValue;
 }
@@ -46,4 +57,10 @@ async function getDailyPorfolioData(): Promise<any> {}
 function convertDateFormat(inputDate: string): string {
     const [month, day, year] = inputDate.split('/');
     return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+}
+
+function convertDateToString(inputDate: Date): string {
+    const month = (inputDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = inputDate.getDate().toString().padStart(2, '0');
+    return `${inputDate.getFullYear()}-${month}-${day}`;
 }
