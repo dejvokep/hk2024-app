60ee185e6ccd658f0df29d00832074c9a201c26a
TomasHutnan <tomas.hutnan147@gmail.com>
2024-04-06 17:45:46 +0200

    create mongo struct

--- lib/db_mongo.ts
+++ lib/db_mongo.ts
@@ -1,5 +1,94 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 // Replace with your connection string and database name
-const mongo = new MongoClient(process.env.MONGO_URI || "");
+const client = new MongoClient(process.env.MONGO_URI || "");
 
+export async function createUser(_id: string, email: string, name: string, surname: string, balance: number = 0, questionnaire: Map<string, any> = new Map, portfolio: Map<string, number> = new Map, transactions: [] = []): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const info_collection = db.collection("userdata");
+    await info_collection.insertOne({ _id: new ObjectId(_id), email, name, surname, balance, questionnaire});
+
+    const portfolios_collection = db.collection("portfolios");
+    await portfolios_collection.insertOne({ _id: new ObjectId(_id), portfolio});
+    
+    const transactions_collection = db.collection("transactions");
+    await transactions_collection.insertOne({ _id: new ObjectId(_id), transactions});
+
+    await client.close();
+}
+
+export async function updateUserQuestionnaire(_id: string, questionnaire: Map<string, any>): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const info_collection = db.collection("userdata");
+    await info_collection.updateOne({ _id: new ObjectId(_id) }, { $set: { questionnaire } });
+
+    await client.close();
+}
+
+export async function getUserQuestionnaire(_id: string): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const info_collection = db.collection("userdata");
+    const info = await info_collection.findOne({ _id: new ObjectId(_id) });
+
+    await client.close();
+    return info?.questionnaire; // Add null check using the optional chaining operator
+}
+
+export async function getUserInfo(_id: string): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const info_collection = db.collection("userdata");
+    const info = await info_collection.findOne({ _id: new ObjectId(_id) });
+
+    await client.close();
+    return info;
+}
+
+export async function getUserPortfolio(_id: string): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const portfolios_collection = db.collection("portfolios");
+    const portfolio = await portfolios_collection.findOne({ _id: new ObjectId(_id) });
+
+    await client.close();
+    return portfolio;
+}
+
+export async function updateUserStock(_id: string, stock: string, quantity: number): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const portfolios_collection = db.collection("portfolios");
+    await portfolios_collection.updateOne({ _id: new ObjectId(_id) }, { $set: { [stock]: quantity } });
+
+    await client.close();
+}
+
+export async function getTransactions(_id: string): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const transactions_collection = db.collection("transactions");
+    const transactions = await transactions_collection.findOne({ _id: new ObjectId(_id) });
+
+    await client.close();
+    return transactions;
+}
+
+export async function addTransactions(_id: string, transaction: any): Promise<any> {
+    await client.connect();
+    const db = client.db();
+
+    const transactions_collection = db.collection("transactions");
+    await transactions_collection.updateOne({ _id: new ObjectId(_id) }, { $push: { transactions: transaction } });
+
+    await client.close();
+}
