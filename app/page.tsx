be4ac09c1e259e7098c880b50d3763a65b42f1a9
dejvokep <37455929+dejvokep@users.noreply.github.com>
2024-04-06 14:33:32 +0200

    Redirect to zone if logged in.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import {Button} from "@/components/ui/button";
-import Image from "next/image";
 import Logo from "@/components/Logo";
 import Link from "next/link";
+import {getSession} from "@auth0/nextjs-auth0";
+import {redirect} from "next/navigation";
 
-export default function Home() {
-  return (
-      <main className="grid w-full min-h-screen place-items-center">
-              <div className={"flex flex-col place-items-center space-y-[20vh]"}>
-                  <Logo className={"fill-primary h-32"}/>
-                  <Button className={"min-w-[250px]"} asChild>
-                      <Link href={"/api/auth/login"}>Login</Link>
-                  </Button>
-              </div>
-      </main>
-  );
+export default async function Home() {
+    const session = await getSession();
+
+    if (session)
+        return redirect("/zone")
+
+    return (
+        <main className={"flex justify-center w-full min-h-screen py-[100px]"}>
+            <div className={"flex flex-col justify-between"}>
+                <Logo className={"fill-primary h-32 mx-auto"}/>
+                <Button className={"min-w-[300px] py-6"} asChild>
+                    <Link href={"/api/auth/login"}>Login</Link>
+                </Button>
+            </div>
+        </main>
+    );
 }
