5d0c73fb1a3c051bcae89b084f5930dde636393b
TomasHutnan <tomas.hutnan147@gmail.com>
2024-04-07 04:02:34 +0200

    Update populate_history.py

--- python_db_funcs/populate_history.py
+++ python_db_funcs/populate_history.py
@@ -19,10 +19,25 @@ portfolios = db.get_collection('portfolios')
 objid = bson.ObjectId("66116391ee779e1b4fd68379")
 #portfolios.update_one({"_id": objid}, { "$push": { "history": {"date": "test", "value": 10} } })
 
-value = 1000000
-date = datetime.datetime.now() - datetime.timedelta(days=365)
-for x in range(1460):
-    value = value + (value * random.uniform(-0.003, 0.005))
-    portfolios.update_one({"_id": objid}, { "$push": { "history": {"date": date.strftime("%Y-%m-%dT%H:%M:%S"), "value": round(value, 2)} } })
+""" value = 341210.94
+date = datetime.datetime.now()
+history = []
+steps = 12
+for _ in range(365*steps):
+    value = value + (value * random.uniform(-0.001, 0.0008))
+    if random.randint(0, 1000) == 0:
+        value = value - value*0.3
+    elif value%(steps*10) == 0 and random.randint(0, 2) == 0:
+        value = value + random.choice([-0.07, 0.03]) * value
+    date = date - datetime.timedelta(hours=24//steps)
+    history.append({"date": date.strftime("%Y-%m-%dT%H:%M:%S"), "value": round(value, 2)})
+
+print(history[::-1], file=open("history.txt", "w")) """
+
+history = []
+with open("history.txt", "r") as f:
+    history = f.readline()
+    history = eval(history)
+
+portfolios.update_one({"_id": objid}, { "$set": { "history": history } })
     #print({"date": date.strftime("%Y-%m-%dT%H:%M:%S"), "value": value})
-    date = date + datetime.timedelta(hours=6)
