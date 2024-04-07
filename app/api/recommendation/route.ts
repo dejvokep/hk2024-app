c29af7e6242073ecebc38c2fcc56e72141445450
dejvokep <37455929+dejvokep@users.noreply.github.com>
2024-04-07 11:43:55 +0200

    fix

--- app/api/recommendation/route.ts
+++ app/api/recommendation/route.ts
@@ -7,10 +7,10 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
     const session = await getSession()
     if (!session)
         return new NextResponse(undefined, {status: 403})
-    const info = await getUserInfo(session.user.sub.substring(6))
-    return new NextResponse(JSON.stringify(info), {status: 200})
+    return new NextResponse(JSON.stringify(session.user.sub), {status: 200})
 /*
 
+    const info = await getUserInfo(session.user.sub.substring(6))
     if (!info.questionnaire)
         return new NextResponse(JSON.stringify([]), {status: 200})
 
